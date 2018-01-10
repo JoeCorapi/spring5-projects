814cdd638fb6f74372752e57f75eadd40d6e06ad
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-01-10 15:05:02 -0500

    Basic pojo classes

---
+++ src/main/java/model/Author.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Author {
+
+    private String firsName;
+    private String lastName;
+
+    private Set<Book> books = new HashSet<>();
+
+    public Author(String firsName, String lastName) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
