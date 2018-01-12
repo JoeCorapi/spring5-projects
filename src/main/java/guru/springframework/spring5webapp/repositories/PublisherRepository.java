3a19ef6d36a61645dccfce10b2296ff2db958984
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-01-12 13:30:22 -0500

    Simple Thymeleaf books view

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -1,6 +1,5 @@
-package guru.springframework.spring5webapp.model.repositories;
+package guru.springframework.spring5webapp.repositories;
 
-import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.model.Publisher;
 import org.springframework.data.repository.CrudRepository;
 
